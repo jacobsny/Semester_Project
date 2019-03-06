fe0b2e0251f8d5ee6b734bd47d05eb5b47b91dbf
Stephen Malinowski <smalinow@buffalo.edu>
2019-03-06 13:09:30 -0500

    a final version of a python version, am going to swap to JS though due to web requirements

--- src/jacobsabitch/readthetitle.py
+++ src/jacobsabitch/readthetitle.py
@@ -6,10 +6,10 @@ pygame.init()
 def game(xaxis, yaxis):
     kill = True
     screen = pygame.display.set_mode((500, 500)) #update for ratio aspects in the future#
-    screen.fill([255, 255, 255])
+    screen.fill([255, 255, 255]) #TEST#
     x = 50
     y = 50
-    once = 0
+    holders = [[120, 120], [430, 130]]
     later = [x, y] #until i figure out how to have a val update in a function
     while kill:
         pygame.time.delay(100)
@@ -17,28 +17,39 @@ def game(xaxis, yaxis):
             if run.type == pygame.QUIT:
                 pygame.quit()
                 kill = False
-        l = window(xaxis, yaxis, later, screen)
-        later = l
+        l = window(xaxis, yaxis, later, screen, holders)
+        later = l[0]
+        holders = l[1]
 
 def collectbles(screen, holders, x, y):
         normal = (255, 0, 255)
+        why = []
+        counter = 0
         for i in holders:
             pygame.draw.rect(screen, normal, (i[0], i[1], 6, 12))
+            why.append([i[0], i[1]])
+            if i[0] == x and i[1] == y: #THIS IS A BAREBONES FUNCTION FOR DEMO ONE ONLY#
+                pygame.draw.rect(screen, (255, 255, 255), (i[0], i[1], 6, 12))
+                why.remove([i[0], i[1]])
+                counter + 1
+        return why
 
 
-def window(xaxis, yaxis, later, screen):
+
+
+def window(xaxis, yaxis, later, screen, holders):
     pygame.display.set_caption("tester")
     x = later[0]
     y = later[1]
     vel = 10
     updatelater = playerprofile(x, y, vel)
     screen.fill([255, 255, 255])
-    holders = [[120, 120], [130, 130]]  ## Just to see if they display, the function in the backend will remove them
-    collectbles(screen, holders, x, y)
+    ## Just to see if they display, the function in the backend will remove them
+    why = collectbles(screen, holders, updatelater[0], updatelater[1])
     pygame.draw.circle(screen, (0, 255, 17), (updatelater[0], updatelater[1]), updatelater[2])
     dirt = (updatelater[0], updatelater[1])
     pygame.display.update()
-    return dirt
+    return [dirt, why]
 
 
 
