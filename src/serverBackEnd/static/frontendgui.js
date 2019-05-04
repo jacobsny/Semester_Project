b3a1a33c80e4ac4136f6e0bbabac134810ed5297
Stephen Malinowski <smalinow@buffalo.edu>
2019-05-04 19:12:01 -0400

    So, sockets bugging out but front end should be go to go, once you get back from your rochester trip we need to talk about this though

--- src/serverBackEnd/static/frontendgui.js
+++ src/serverBackEnd/static/frontendgui.js
@@ -1,64 +1,158 @@
+var killState = false;
+var location = [0,0];
+var speed = 5.0;
+var nameid = "";
+var Food = [];
+var Players = [];
+var color = "cyan"
+
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-        var jacob = why(this.response);
-        console.log("Connected")
-
+        var parsed = JSON.parse(this.response);
+        location = parsed["location"]
+        nameid = parsed["nameid"]
+        convertingfromJson(this.response)
     }
 };
-xhttp.open("GET", "/playerupdate");
+xhttp.open("GET", "/newPlayerEndpoint");
 xhttp.send();
 
+socket = io.connect({transports: ['websocket']});
 
-function why(x){
-    console.log(10)
-    var converted = JSON.parse(x)
-    return converted
+setupSocket();
+
+function convertingfromJson(parsed){
+    var parsed = JSON.parse(x);
+    var players = {};
+    var food = [];
+    var players = {};
+    var food = [];
+    for (var ind of parsed){
+        if (ind == "kill"){
+            if (ind["kill"] == false){
+                killState = false
+            }
+            else if(ind["kill"] == "False"){
+                killState = false
+            }
+            else{
+                killState = true
+            }
+        }
+        else if(ind == "locations"){
+            for (var playorfood of ind){
+                if(playorfood[0] === "f" || playorfood[0] === 'f'){
+                    food.push(ind[playorfood])
+                }
+                else{
+                    players.push(ind[playorfood])
+                }
+            }
+        }
+    }
+    return [food, players]
 }
 
-function printer(x){
-    console.log(10)
-    return x
+function loadVisual(gameStateDict){
+    killState = gameStateDict["kill"]
+    //set up everything for visual
 }
 
-function setup(x) {
+function setupSocket() {
+    socket.on('connect', function (event) {
+        socket.send('Hello Server!');
+    });
+    socket.on('message', function (event) {
+        // console.log(event);
+        const gameState = JSON.parse(event);
+        console.log(gameState);
+        loadVisual(gameState);
+    });
+}
+
+
+function setup() {
     var aspectx = 900
     var aspecty = 900
-    var zed = 23
-    var ided = 223442
     createCanvas(aspectx, aspecty);
     var x = aspectx/2
     var y = aspecty/2
-    circles = new themcircles("This_will_have_a_json_in_part_two_from_backend")
-    user = new User("cyan", x, y, zed. ided);
+    circles = new themcircles()
+    if (killState == false) {
+        user = new User(color);
+    }
 }
 
-function draw(x) {
+function draw() {
     background(255,255,255)
-    circles.minidot();
-    keyisPress();
+    circles.minidot(Food);
+    circles.otherplayers(Players)
+    user.show(location[0], location [1], 30)
 }
 
-function keyisPress(){
-    if ((keyCode === UP_ARROW)) {
-        user.up()
-        console.log("W")
+while (!killState){
+    document.addEventListener('keydown', function(e){
+        if ((e.key === '38')) {
+            location[1] += speed;
+            console.log("UP")
 
-    }
-    if (keyCode === DOWN_ARROW) {
-        user.down()
-        console.log("s")
-    }
-    if (keyCode === LEFT_ARROW) {
-        user.left()
-        console.log("L")
-    }
-    if (keyCode === RIGHT_ARROW) {
-        user.right()
-        console.log("R")
-    }
-    else {
-        user.show();
-    }
+        }
+        if (e.key === '40') {
+            location[1] -= speed;
+            console.log("DOWN")
+        }
+        if (e.key === '37') {
+            location[0] -= speed;
+            console.log("LEFT")
+        }
+        if (e.key === '39') {
+            location[0] += speed;
+            console.log("RIGHT")
+        }
+        if ((e.key === 'w')) {
+            location[1] += speed;
+            console.log("UP")
+
+        }
+        if (e.key === 's') {
+            location[1] -= speed;
+            console.log("DOWN")
+        }
+        if (e.key === 'a') {
+            location[0] -= speed;
+            console.log("LEFT")
+        }
+        if (e.key === 'd') {
+            location[0] += speed;
+            console.log("RIGHT")
+        }
+    });
+    var obj = '{'
+        +'"nameid" : '
+        + nameid
+        +'"location" : '
+        + location.toString()
+        +'}';
+    socket.emit('update', obj)
 }
+
+//
+// function keyisPress(){
+//     if ((keyCode === UP_ARROW)) {
+//         user.up()
+//     }
+//     if (keyCode === DOWN_ARROW) {
+//         user.down()
+//     }
+//     if (keyCode === LEFT_ARROW) {
+//         user.left()
+//     }
+//     if (keyCode === RIGHT_ARROW) {
+//         user.right()
+//     }
+//     else {
+//         user.show();
+//     }
+// }
--- src/serverBackEnd/static/themdots.js
+++ src/serverBackEnd/static/themdots.js
@@ -1,19 +1,17 @@
-templists = [[420, 120], [120, 420], [320, 120]];
-function themcircles(x){ //so the x is going to be a json with a list of dots... go figure//
-    if (k1 == true){
-        templists = [[120,420], [320, 120]]
-    }
-    if (k2 == true){
-        templists = [[420,120], [320, 120]]
-    }
-    if (k3 == true){
-        templists = [[420,120], [120, 420]]
-    }
-    this.minidot = function(){
-        for (var ind in templists) {
+function themcircles(){ //so the x is going to be a json with a list of dots... go figure//
+    this.minidot = function(food){
+        for (var ind in food) {
             fill(color(122, 0, 122))
-            var i = templists[ind]
+            var i = food[ind]
             ellipse(i[0], i[1], 10, 10);
+            //console.log(ind[0])//
+        }
+    }
+    this.otherplayers = function(players){
+        for (var ind in players) {
+            fill(color(22, 33, 122))
+            var i = players[ind]
+            ellipse(i[0], i[1], 20, 20);
             //console.log(ind[0])//
         }
     }
